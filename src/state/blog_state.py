9d076eae0443c8920dd6a362a94acbbafb19ea54
Amit Kumar <amitkuamr981>
2025-07-24 06:41:17 +0530

    add blog_state.py script

---
+++ src/state/blog_state.py
@@ -0,0 +1,15 @@
+#importing libraries
+from typing_extensions import TypedDict
+from pydantic import BaseModel,Field
+
+#define class
+class Blog(BaseModel):
+    title:str=Field(discription='title of the blog')
+    content:str=Field(discription='The main content of the post blog')
+
+class StateBlog(TypedDict):
+    topic:str=Field(discription='The topic of the blog')
+    blog:Blog=Field(discription='The blog objecy containing the title and content of blog')
+    language:str=Field(discription='Current language of blog')
+    
+    
