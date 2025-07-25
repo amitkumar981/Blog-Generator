0d117326d62214834d62e44802088e729ca75d4e
Amit Kumar <amitkuamr981>
2025-07-25 06:36:15 +0530

    update graph_builder.py

--- src/graph/graph_builder.py
+++ src/graph/graph_builder.py
@@ -22,19 +22,44 @@ class Graph_Builder:
         self.graph_builder.add_edge('content_creation',END)
         
         return self.graph_builder
+    def graph_fot_translation(self):
+        self.blog_node_object=BlogNode(self.llm)
 
-    def compile_graph(self,usecase):
-        if usecase=="topic":
-           self.build_topic_graph()
+        #add node
+        self.graph_builder.add_node('title_creation',self.blog_node_object.title_generation)
+        self.graph_builder.add_nodel('content_creation',self.blog_node_object.content_generation)
+        self.graph_builder.add_node('hindi_translation',lambda state:self.blog_node_object.translation({**state,'current_lanuage':'Hindi'}))
+        self.graph_builder.add_node('hindi_translation',lambda state:self.blog_node_object.translation({**state,'current_lanuage':'French'}))
+        self.graph_builder.add_node('route',self.blog_node_object.route_decision)
 
-        return self.graph_builder.compile()
+        #add edge
+        self.graph_builder.add_edge(START,'title_creation')
+        self.graph_builder.add_edge('title_creation','content_creation')
+        self.graph_builder.add_edge('content_creation','route')
+
+        #add condition edges
+        self.graph_builder.add_conditional_edges('route',
+                                                 {'Hindi':'hindi_translation',
+                                                  'French':'french_translation'})
+        #add end edges
+        self.graph_builder.add_edge('hindi_translation',END)
+        self.graph_builder.add_edge('french_translation',END)
+
+
+        def compile_graph(self,usecase):
+            if usecase=="topic":
+                self.build_topic_graph()
+            elif   usecase=='language':
+                self.graph_for_translation()
+            return self.graph_builder.compile()
 
 #code for langsmith studio
 llm=GROQLLM().get_llm()
 
 graph_builder=Graph_Builder(llm)
 
-graph=graph_builder.build_topic_graph().compile()
+graph=graph_builder.graph_fot_translation().compile()
+
 
 
     
